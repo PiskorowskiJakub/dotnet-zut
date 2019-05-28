884fbe7db4c8eb979e2e828ce7346af9028fd1ea
Jakub Piskorowski <jakub9616@gmail.com>
2019-05-28 13:54:29 +0200

    Initialize with push notifications

---
+++ dotnetzut/dotnetzut/dotnetzut.Android/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace dotnetzut.Droid
+{
+    class Constants
+    {
+        
+
+    public const string ListenConnectionString = "Endpoint=sb://dotnet-zut.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=1qjd2hvrdPztrqJ5knOORedv3uFogrbldJFXzehAnHE=";
+    public const string NotificationHubName = "dotnet-zut-hub";
+    }
+}
